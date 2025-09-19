95ab05d76fbf41a138a5ef8f37da7458819133d0
Subiksha Anand <subikshamatcs@gmail.com>
2025-09-19 19:58:59 +0530

    Update check.py

--- api/check.py
+++ api/check.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-model_path = r"C:\Users\subik\OneDrive\Document\PROJECT\potato\saved_model\converted_model.keras" # Change to your model's path
+model_path = "saved_model/converted_model.keras" # Change to your model's path
 model = tf.keras.models.load_model(model_path)
 
 if model is None:
